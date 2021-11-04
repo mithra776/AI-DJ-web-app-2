0e29061aad98df91177028851214fec268814868
mithra776 <85687745+mithra776@users.noreply.github.com>
2021-11-04 12:55:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+song = "";
+song2 = "";
+
+function preload()
+{
+    song = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(450 , 450);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+    image(video , 0 , 0 , 450 , 450);
+}
+
+
